4b2eea8800dc2a57a0a92ab6be12b93561fb26be
Joeleen Gaffney <99375501+joeleengaffney@users.noreply.github.com>
2022-02-10 18:43:07 -0500

    Add files via upload

---
+++ HW4-9a.R
@@ -0,0 +1,15 @@
+u = 1:9
+j = 0
+i = 1
+while (i <= 1000000) {
+  result = sample(u, replace = TRUE, 3)
+  if (1 %in% result || 2 %in% result || 3 %in% result) {
+    if (4 %in% result || 5 %in% result || 6 %in% result) {
+      if (7 %in% result || 8 %in% result || 9 %in% result) {
+        j = j + 1
+      }
+    }
+  }
+  i = i + 1
+}
+print(j/1000000)
